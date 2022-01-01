d78f4ef723d49c2f3360454f7b064fed33ce1d04
grinnan <cgrinnan@gmail.com>
2021-12-31 16:41:04 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.magneticForce(Dimension.Strength))
+    basic.pause(100)
+})
+basic.forever(function () {
+	
+})
+control.inBackground(function () {
+	
+})
